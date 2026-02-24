0792351c7dc2163823ab80bdcfa5ccf1ddec6644
huhsiyoung <july433@naver.com>
2026-02-24 21:19:12 +0900

    first commit

---
+++ src/main/java/test/actions/git/gitactionstest/GitActionsTestApplication.java
@@ -0,0 +1,13 @@
+package test.actions.git.gitactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/actions/git/gitactionstest/GitActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.actions.git.gitactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
